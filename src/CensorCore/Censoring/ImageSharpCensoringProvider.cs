fedd590747360ddcc35cff9b9016b3ca0b18d2f0
Silvered Gold <35001040+silveredgold@users.noreply.github.com>
2022-03-16 19:35:51 +1000

    Add prototype result merging/transforming;

--- src/CensorCore/Censoring/ImageSharpCensoringProvider.cs
+++ src/CensorCore/Censoring/ImageSharpCensoringProvider.cs
@@ -26,7 +26,35 @@ namespace CensorCore.Censoring
         public async Task<CensoredImage> CensorImage(ImageResult image, IResultParser? parser = null) {
             var img = image.ImageData.SourceImage;
             var censorEffects = new Dictionary<int, List<Action<IImageProcessingContext>>>();
-            foreach (var match in image.Results.OrderBy(r => r.Box.Width * r.Box.Height))
+            var matches = image.Results.GroupBy(r => r.Label).ToList();
+            var transformedMatches = new List<Classification>();
+            foreach (var labelGroup in matches)
+            {
+                if (labelGroup.Count() > 1 && labelGroup.All(r => labelGroup.Any(l => r.Box.ToRectangle().IntersectsWith(l.Box.ToRectangle())))) {
+                    foreach (var pair in labelGroup.Chunk(2))
+                    {
+                        if (pair.Count() < 2) {
+                            transformedMatches.Add(pair.First());
+                        }
+                        var rectA = pair.First().Box.ToRectangle();
+                        var rectB = pair.Last().Box.ToRectangle();
+                        var rejections = new List<Predicate<Rectangle>>() {
+                            r => r.Width > rectA.Width * 2F && r.Width > rectB.Width *2F,
+                            r => r.Height > rectA.Height * 2F && r.Height > rectB.Height*2F
+                        };
+                        var unionRect = Rectangle.Union(rectA, rectB);
+                        if (!rejections.All(r => r(unionRect))) {
+
+                            transformedMatches.Add(
+                                new Classification(
+                                    new BoundingBox(unionRect.X, unionRect.Y, unionRect.X+unionRect.Width, unionRect.Y+unionRect.Height), Math.Max(pair.First().Confidence, pair.Last().Confidence), labelGroup.Key));
+                        }
+                    }
+                } else {
+                    transformedMatches.AddRange(labelGroup);
+                }
+            }
+            foreach (var match in transformedMatches.OrderBy(r => r.Box.Width * r.Box.Height))
             {
                 var options = parser?.GetOptions(match, image) ?? this._parser?.GetOptions(match) ?? new ImageCensorOptions(nameof(BlurProvider)) { Level = 10 };
                 var provider = this._providers.FirstOrDefault(p => p.Supports(options.CensorType ?? string.Empty));
